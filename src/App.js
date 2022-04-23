73a2ec4bdea969d3786a3b6060a0bd4be214ece7
Rasujon3 <rasujon3@gmail.com>
2022-04-23 19:54:45 +0600

    note get & post done

--- src/App.js
+++ src/App.js
@@ -5,45 +5,38 @@ import InputForm from "./components/inputForm/InputForm";
 import NoteCard from "./components/noteCard/NoteCard";
 import { useEffect, useState } from "react";
 
-
-
 function App() {
   const [notes, setNotes] = useState([]);
+  const [isReload, setIsReload] = useState(false);
 
   useEffect(() => {
-
-   fetch("notes.json")
-   .then(res=>res.json())
-   .then(data=>setNotes(data))
-
-    
-  }, []);
+    fetch("http://localhost:5000/notes")
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
+  }, [isReload]);
   /*
 1. here there will be a function named handleSearch
 to handle search by query, and it will be passed as props to header
+*/
 
-  */
-  
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const queryText = event.target.searchText.value;
 
+    if (queryText) {
+      fetch(`http://localhost:5000/notes?userName=${queryText}`)
+        .then((res) => res.json())
+        .then((data) => setNotes(data));
+    }
+  };
 
-
-
-
-
-  
-/*2. here there will be a function named handleDelete
+  /*2. here there will be a function named handleDelete
 to delete a note, and it will be passed as props to NoteCard that will be triggered using delete button.
  */
 
-
-
-
-
-
-
-
-
-
+  const handleDelete = (id) => {
+    console.log("handleDelete", id);
+  };
 
   /*
 3. there will be a function named handleUpdate
@@ -51,28 +44,39 @@ to delete a note, and it will be passed as props to NoteCard that will be trigge
    later it will be passed to Update modal using props.
  */
 
-
-
-   
-
-
-
   /*
 4.  there will be a function named handlePost
 to post data to backend, and it will be passed as props to InputFrom.
  */
 
-  
+  const handlePost = (event) => {
+    event.preventDefault();
+    const userName = event.target.userName.value;
+    const textData = event.target.textData.value;
+
+    console.log({ userName, textData });
+    const url = `http://localhost:5000/note`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userName, textData }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setIsReload(!isReload);
+      });
+  };
 
   return (
     <div className="App">
-      <Header  />
-      <InputForm />
+      <Header handleSearch={handleSearch} />
+      <InputForm handlePost={handlePost} />
       <div className="row row-cols-1 row-cols-md-3 g-4 m-2">
         {notes.map((note) => (
-          <NoteCard
-            note={note}
-          />
+          <NoteCard handleDelete={handleDelete} key={note._id} note={note} />
         ))}
       </div>
     </div>
--- src/components/noteCard/NoteCard.js
+++ src/components/noteCard/NoteCard.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import UpdateModal from '../updateModal/UpdateModal';
-
+import React from "react";
+import UpdateModal from "../updateModal/UpdateModal";
 
 const customStyles = {
   position: "absolute",
@@ -10,9 +9,7 @@ const customStyles = {
   width: "80px",
 };
 
-const NoteCard = ({ note }) => {
-
-
+const NoteCard = ({ note, handleDelete }) => {
   return (
     <div className="col mt-5" style={{ position: "relative" }}>
       <div className="rounded h-100 color-060930 note-card">
@@ -22,24 +19,24 @@ const NoteCard = ({ note }) => {
         >
           <p className="text-center p-2  fs-2 fw-bold text-dark">
             {" "}
-            {note.user_name.substring(0, 1)}
+            {note.userName.substring(0, 1)}
           </p>
         </div>
         <div className="card-body mt-5">
-          <h5 className="card-title">Author : {note.user_name}</h5>
-          <p className="card-text">{note.text}</p>
+          <h5 className="card-title">Author : {note.userName}</h5>
+          <p className="card-text">{note.textData}</p>
         </div>
         <div className="card-footer d-flex justify-content-center">
           <div>
             <button
               className="color-801336 btn btn-sm mx-2 "
-              
+              onClick={() => handleDelete(note._id)}
             >
               delete
             </button>
           </div>
           {/* <button>update</button> */}
-          <UpdateModal  />
+          <UpdateModal />
         </div>
       </div>
     </div>
